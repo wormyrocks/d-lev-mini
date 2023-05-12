78379ef44a470b2de84afbb0d9046370f4c86f90
Evan Kahn <ekahn@hmc.edu>
2023-05-11 23:48:41 -0400

    gerbolyze scripts

--- release/scripts/functions.sh
+++ release/scripts/functions.sh
@@ -4,26 +4,38 @@
 # https://jeffmcbride.net/programmatic-layout-with-kicad-and-python/
 # https://atomic14.com/2022/10/24/kicad-python-scripting-cheat-sheet-copy.html
 
-retval=""
-WORKDIR=$(mktemp -d)
-
 ###### kicad-cli functions 
-LAYERS="F.Cu,In1.Cu,In2.Cu,B.Cu,F.Mask,B.Mask,F.Silkscreen,B.Silkscreen,F.Paste,B.Paste,Edge.Cuts,"
+LAYERS="F.Cu,In1.Cu,In2.Cu,B.Cu,F.Mask,B.Mask,F.Silkscreen,B.Silkscreen,F.Paste,B.Paste,Edge.Cuts"
 creategerbers() { 
-	mkdir -p ${WORKDIR}/${1}
-	kicad-cli pcb export gerbers -l ${LAYERS} -o ${WORKDIR}/${1} ${1}/${1}.kicad_pcb
-	kicad-cli pcb export drill --format gerber -o ${WORKDIR}/${1}/ ${1}/${1}.kicad_pcb --excellon-separate-th
-	retval=${WORKDIR}/${1}
+	kicad-cli pcb export gerbers -l ${LAYERS} --disable-aperture-macros -o ${2} ${1} 
+	kicad-cli pcb export drill --format gerber -o ${2}/ ${1} --excellon-separate-th
+	NEWGBRDIR=${WORKDIR}/${1}
 }
 ##########################
 
-###### gerbolyze functions 
-source ${GRBDIR}/venv/bin/activate
+###### gerbolyze template creation
+command -v gerbolyze || source ${GRBDIR}/venv/bin/activate
 
-gbtmp() { 
-	projfile=$(basename ${1})
-	gerbolyze template --vector --top ${1} ${1}/${projfile}-top-gerbolyzed.svg
-	gerbolyze template --vector --bottom ${1} ${1}/${projfile}-bottom-gerbolyzed.svg
+#### gerbolyze output
+
+vectorizer=hex-grid
+vectorizer=square-grid
+vectorizer=binary-contours
+vectorizer=poisson-disc
+
+# gerber files
+gbpaste() {
+	modname=$(basename "${1%%.*}")
+	OUTDIR=${WORKDIR}/${modname}-gerbolyzed
+	mkdir -p ${OUTDIR}
+	gerbolyze paste ${2} $(pwd)/${1} ${OUTDIR}
 }
-##########################
 
+# s-exp
+svgf() {
+	modname=$(basename "${1%%.*}")
+	svg-flatten ${1} -o s-exp -b ${vectorizer} --format kicad --sexp-mod-name ${modname} ${modname}.kicad_mod
+	mv ${modname}.kicad_mod d-lev-libraries/art.pretty
+}
+
+##########################
--- release/scripts/release.sh
+++ release/scripts/release.sh
@@ -1,12 +1,49 @@
 #!/bin/bash
+set -xe
+
+# per-run settings
+project=d-lev-tuner
+preview_type=vector
+use_round_tracks=0
 
 GRBDIR=~/GitHub/gerbolyze
-
-set -x
 cd $(dirname "$0")
 . ./functions.sh
 cd ../../
+USE_ROUND_TRACKS=
+[[ "$use_round_tracks" == "0" ]] || USE_ROUND_TRACKS=-rounded
 
-creategerbers d-lev-tuner
-gbtmp ${retval}
-open ${retval}
+PROJECT=${project}${USE_ROUND_TRACKS}
+REPO_ROOT=$(pwd)
+S_EXP_DIR=${REPO_ROOT}/d-lev-libraries/art.pretty
+WORK_DIR=${REPO_ROOT}/release/${PROJECT}_wip
+GERBER_DIR=${WORK_DIR}/gerbers
+SVG_DIR=${WORK_DIR}/svg
+LAYERS="F.Cu,In1.Cu,In2.Cu,B.Cu,F.Mask,B.Mask,F.Silkscreen,B.Silkscreen,F.Paste,B.Paste,Edge.Cuts"
+command -v gerbolyze || source ${GRBDIR}/venv/bin/activate
+
+mkdir -p ${SVG_DIR} ${GERBER_DIR}
+
+SVGTOP=${SVG_DIR}/top.svg
+SVGBOTTOM=${SVG_DIR}/bottom.svg
+
+[[ -e ${SVGTOP} && -e ${SVGBOTTOM} ]] || {
+	creategerbers ${project}/${PROJECT}.kicad_pcb ${GERBER_DIR}
+	[[ -e ${SVGTOP} ]] || gerbolyze template --${preview_type} --top ${WORK_DIR} ${SVGTOP}
+	[[ -e ${SVGBOTTOM} ]] || gerbolyze template --${preview_type} --bottom ${WORK_DIR} ${SVGBOTTOM}
+	echo ok, svg files created. run this script again to paste or generate s-expressions.
+	open ${SVG_DIR}
+	exit 0
+}
+
+
+vectorizer=hex-grid
+vectorizer=square-grid
+vectorizer=binary-contours
+vectorizer=poisson-disc
+
+ztmp=$(mktemp -u).zip
+gerbolyze paste ${GERBER_DIR} ${SVGTOP} ${ztmp}
+gerbolyze paste ${ztmp} ${SVGBOTTOM} ${WORK_DIR}/${PROJECT}_pasted.zip
+#svg-flatten ${SVGBOTTOM} -o s-exp -b ${vectorizer} --format kicad --sexp-mod-name ${project}-bottom ${WORK_DIR}/${project}-bottom.kicad_mod
+#svg-flatten ${SVGTOP} -o s-exp -b ${vectorizer} --format kicad --sexp-mod-name ${project}-top ${WORK_DIR}/${project}-top.kicad_mod
